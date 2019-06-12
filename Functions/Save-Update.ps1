53894b86818a44220bd66f3329593944500319d4
Fonta <gianni@fonta.nl>
2019-06-13 00:04:36 +0200

    Proxy fix and some code markup changes

--- Functions/Save-Update.ps1
+++ Functions/Save-Update.ps1
@@ -15,49 +15,64 @@ function Save-Update {
 
     Begin {
         $ScriptParentFolder = (Get-Item -Path $PSScriptRoot).Parent
-        $ConfigFile = Join-Path -Path $ScriptParentFolder -ChildPath "Config.json"
+        $ConfigFile = Join-Path -Path $ScriptParentFolder.FullName -ChildPath "Config.json"
         
         if (Test-Path -Path $ConfigFile -PathType Leaf -ErrorAction SilentlyContinue) {
-            $Config = (Get-Content -Path $ConfigFile) -join "`n" | ConvertFrom-Json
-        }        
+            $Config = Get-Content -Path $ConfigFile | ConvertFrom-Json
+        }
 
-        $Download = $true
         $Overwrite = $false
         $SavePath = Join-Path -Path $Path -ChildPath $Update.Note
     
         $SavePathExists = Test-Path -Path $SavePath
-        if ($SavePathExists -and (-not $Force.IsPresent)) {
-            Write-Log "Update already seems to exist. Force param NOT defined. " -NoNewLine -NoTime
-            Write-Log "Skipping." -NoTime -ForegroundColor "Yellow"
-            $Download = $false
+        if ($SavePathExists) {
+            $SavePathFiles = Get-ChildItem -Path $SavePath -File
+
+            if ($SavePathFiles.Count -gt 0) {
+                if ($SavePathExists -and (-not $Force.IsPresent)) {
+                    $Skipped = $true
+                    Write-Log "Update already seems to exist. Force param NOT defined. " -NoNewLine -NoTime
+                    Write-Log "Skipped." -NoTime -ForegroundColor "Yellow"
+                }
+                if ($SavePathExists -and $Force.IsPresent) {
+                    $Overwrite = $true
+                    Write-Log "Update already seems to exist. Force param defined. " -NoNewLine -NoTime
+                    Write-Log "Overwriting... " -NoNewLine -NoTime -ForegroundColor "Yellow"
+                }
+            }
+            else {
+                Write-Log "Destination folder already exists, but seems to be empty... Continuing with downloading..." -NoNewLine -NoTime
+            }
         }
-        if ($SavePathExists -and $Force.IsPresent) {
-            Write-Log "Update already seems to exist. Force param defined. " -NoNewLine -NoTime
-            Write-Log "Overwriting... " -NoNewLine -NoTime -ForegroundColor "Yellow"
-            $Overwrite = $true
-        }
-        if (-not $SavePathExists) {
+        else {
             $null = New-Item -Path $SavePath -ItemType Directory
+        }
+
+        $SaveParams = @{
+            Updates = $Update
+            Path    = $SavePath
+            Force   = $Overwrite
+            # Verbose = $true
+        }
+
+        if ($UseProxy.IsPresent) {
+            $SaveParams.Proxy = "$($Config.ProxyUrl):$($Config.ProxyPort)"
         }
     }
 
     Process {
-        if ($Download) {
-            $SaveParams = @{
-                Path  = $SavePath
-                Force = $Overwrite
-                # Verbose = $true
-            }
-    
-            if ($UseProxy.IsPresent) {
-                $SaveParams.Proxy = "$($Config.ProxyUrl):$($Config.ProxyPort)"
-            }
-
-            $SaveResult = $Update | Save-LatestUpdate @SaveParams
+        if ($Skipped) {
+            return
         }
+
+        $SaveResult = Save-LatestUpdate @SaveParams
     }
 
     End {
+        if ($Skipped) {
+            return
+        }
+
         if ($SaveResult) {
             Write-Log "OK!" -NoTime -ForegroundColor "Green"
             Write-Log "Update successfully downloaded to $($SaveResult.Target)"
